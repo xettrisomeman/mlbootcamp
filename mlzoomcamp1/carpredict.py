3fdcc96824841ff97643a324ea53a94db4bfe5a9
xettrisomeman <webizmoxx@gmail.com>
2021-09-08 09:20:22 +0545

    Polished the project , finished homework1

--- mlzoomcamp1/carpredict.py
+++ mlzoomcamp1/carpredict.py
@@ -3,35 +3,64 @@ import numpy as np
 
 df = pd.read_csv('data.csv')
 
+
+# print("The head of the data")
 # print(df.head())
 
 def get_car_average_price(dataframe, car_name):
+
+    # get bmw cars "msrp" values
     bmw_cars = dataframe[dataframe['Make'] == car_name]['MSRP'].values
+
+    # iterate over bmw prices
     bmw_cars_price = [car for car in bmw_cars]
+
+    # return average price
     return sum(bmw_cars_price)/bmw_cars.shape[0]
 
-#Q3. print(get_car_average_price(df, "BMW"))
+print(f"Question 3.")
+print(get_car_average_price(df, "BMW"))
+
 
 def get_missing_values(dataframe, date):
+
+    # get missing data before and after 2015
     after_2015 = dataframe[dataframe['Year'] >= date]
+
+    # check null values
     get_empty_column = after_2015.isna().sum().loc['Engine HP']
+
+    # return null value after 2015 and equal to 2015
     return get_empty_column
 
-#Q4. print(get_missing_values(df, 2015))
+print(f"Question 4.")
+print(get_missing_values(df, 2015))
 
 
 def get_average_and_fill(dataframe):
+
+    # check mean value of engine_HP
     engine_HP_mean = dataframe.describe().loc['mean', 'Engine HP']
+
+    #fill the missing value with mean value
     fill_engine_HP = dataframe.fillna(engine_HP_mean, axis=1)
 
+    # get mean value after filling the data
     get_mean_again = dataframe.describe().loc['mean', 'Engine HP']
+
+    # return before mean and after filling the null values
     return (f"Before mean : {round(engine_HP_mean)}", f"After mean: {round(get_mean_again)}")
 
-# Q5. print(get_average_and_fill(df))
+print(f"Question 5.")
+print(get_average_and_fill(df))
 
 
 def get_roll_royce(dataframe, car_name, columns):
+
+    # get roll royce
     get_royce = dataframe[dataframe['Make'] == car_name]
+
+    # get the required columns
     get_columns = get_royce[columns]
 
     # drop duplicates rows
@@ -52,7 +81,8 @@ def get_roll_royce(dataframe, car_name, columns):
     return inv_xTx, sum_InvX, x
 
 inv_xTx, sum_InvX, x = get_roll_royce(df, "Rolls-Royce", ['Engine HP', 'Engine Cylinders', 'highway MPG'])
-# print(sum_InvX)
+print("Question 6.")
+print(sum_InvX)
 
 
 
@@ -79,5 +109,5 @@ def get_normal_equation(x, inv_xTx, y):
     
     return w2[0]
 
-
-# Q7. print(get_normal_equation(x, inv_xTx, y))
+print("Question 7.")
+print(get_normal_equation(x, inv_xTx, y))
